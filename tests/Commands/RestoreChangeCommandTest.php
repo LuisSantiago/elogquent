8a86219b726ca86c214edbbe6602cf9b6c988005
blad <xtravagancia@gmail.com>
2025-05-10 21:13:02 +0200

    Feat/code coverage (#10) (#12)
    * Add tests and try catches for db queries
    
    * Add test coverage
    
    * Fix actions
    
    ---------
    
    Co-authored-by: Luis S. <l.santiago@sellboost.com>

--- tests/Commands/RestoreChangeCommandTest.php
+++ tests/Commands/RestoreChangeCommandTest.php
@@ -2,6 +2,34 @@
 
 use Elogquent\Models\ElogquentEntry;
 
+it('elogquent is not enabled and return a message', function () {
+    config(['elogquent.enabled' => false]);
+
+    $this->artisan('elogquent:restore-changes')
+        ->expectsOutputToContain('enable elogquent in config/elogquent.php')
+        ->assertExitCode(0);
+});
+
+it('elogquent is enabled but has no entries', function () {
+    $this->artisan('elogquent:restore-changes')
+        ->expectsOutputToContain('No models with changes found.')
+        ->assertExitCode(0);
+});
+
+it('the user is trying to restore a non-existent id', function () {
+    $modelClass = ElogquentEntry::class;
+    ElogquentEntry::factory()->create([
+        'model_id' => 1,
+        'model_type' => $modelClass,
+    ]);
+
+    $this->artisan('elogquent:restore-changes')
+        ->expectsChoice('Select the Model you want to restore:', $modelClass, [$modelClass => $modelClass])
+        ->expectsQuestion('Which model id would you like to restore?', 2)
+        ->expectsOutputToContain('with id 2 not found')
+        ->assertExitCode(0);
+});
+
 it('the command select a model and a id to restore', function () {
     $modelClass = ElogquentEntry::class;
     ElogquentEntry::factory()->create([
